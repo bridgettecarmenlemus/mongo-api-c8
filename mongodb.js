5407d8c5a1f748f2caa697552252a3d56a712521
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-10-11 15:58:22 -0400

    inserted my own movie

--- mongodb.js
+++ mongodb.js
@@ -6,6 +6,22 @@ const client = new MongoClient(uri)
 const db = client.db("sample_mflix")
 const moviesCollection = db.collection("movies")
 
-let query = {title: { $regex: /terminator/i }}
-let firstMovie = await moviesCollection.find(query).limit(3).toArray()
-console.log(firstMovie)
+// let query = {title: { $regex: /terminator/i }}
+// let movieArray = await moviesCollection.find(query).limit(3).toArray()
+// let firstMovie = movieArray[0]
+
+// for (let i=0; i < movieArray.length; i++) {
+//     console.log(movieArray[i].title)
+// }
+// // movieArray[i]
+// // console.log(firstMovie.title)
+// console.log(`There are ${movieArray.length} movies`)
+
+const newMovie = {
+    title: "The Boca Code story",
+    rating: "R",
+    genre: ['Comedy'],
+    relaeaseDate: "2022/12/16",
+}
+const results = await moviesCollection.insertOne(newMovie)
+console.log("Results of Insert", results)
